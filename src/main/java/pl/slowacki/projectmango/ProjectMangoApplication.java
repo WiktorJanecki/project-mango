c04d3862b72504702b74d0afa60d628de3c8d9e7
Wiktor Janecki <j.wiktor05@interia.pl>
2022-11-12 19:14:53 +0100

    Migrate to gradle and Import Spring

---
+++ src/main/java/pl/slowacki/projectmango/ProjectMangoApplication.java
@@ -0,0 +1,14 @@
+package pl.slowacki.projectmango;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMangoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMangoApplication.class, args);
+	}
+
+}
+s
---
+++ src/test/java/pl/slowacki/projectmango/ProjectMangoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.slowacki.projectmango;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMangoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
